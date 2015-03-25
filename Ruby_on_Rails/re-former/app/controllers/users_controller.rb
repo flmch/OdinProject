8c37fe21ae7a8863ac72e5567aa77a6316d3b992
Your Name Comes Here <xizhangmit@gmail.com>
2015-03-25 01:26:38 -0400

    Finish form tutorial

--- Ruby_on_Rails/re-former/app/controllers/users_controller.rb
+++ Ruby_on_Rails/re-former/app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
 	def new
 		@user = User.new
+		@alluser = User.all
 	end
 
 	def create
-		fail
-		#@user = User.find(params[:id])
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path()
+		else
+			@alluser = User.all
+			render :new
+		end
+	end
+
+private
+
+	def user_params
+		params.permit(:username,
+									 :email,
+									 :password)
 	end
 end
--- Ruby_on_Rails/re-former/app/models/user.rb
+++ Ruby_on_Rails/re-former/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	validates :username, presence: true,
 						 uniqueness: true,
-					     length: { minimum: 4, maximum: 15 }
+					     length: { minimum: 2, maximum: 15 }
 	validates :email,    presence: true		
 	validates :password, presence: true		     
 end
