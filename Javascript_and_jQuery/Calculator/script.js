013c8fe0adf8b54e7f946a153cf0c53bf9992fbb
Your Name Comes Here <xizhangmit@gmail.com>
2015-03-31 23:14:08 -0400

    fixed display problem

--- Javascript_and_jQuery/Calculator/script.js
+++ Javascript_and_jQuery/Calculator/script.js
@@ -3,7 +3,7 @@ window.onload = function(){
 	var cells = table.getElementsByTagName("th");
 
 	var current = 0,last =0;
-	document.getElementById("display").innerHTML = current;
+	document.getElementById("dis").innerHTML = current;
 
 
 
@@ -12,13 +12,13 @@ window.onload = function(){
 			var input = this.innerHTML;
 			if(input == "AC"){
 				current = 0;
-				document.getElementById("display").innerHTML = 0;
+				document.getElementById("dis").innerHTML = 0;
 			}else if(input == "+/-"){
 				current *= -1;
-				document.getElementById("display").innerHTML *= -1;
+				document.getElementById("dis").innerHTML *= -1;
 			}else{
 				current = current == 0 ? input: current+input;
-				document.getElementById("display").innerHTML = current;
+				document.getElementById("dis").innerHTML = current;
 			}
 
 
